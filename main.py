ec04f0b4293382bc7c3301f84178a1e503ae5458
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2020-03-13 23:09:24 +0100

    Added region of interest for lane.jpeg

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import cv2 as cv
 import matplotlib.pylab as plt
 import numpy as np
 
-
 img = cv.imread('lane.jpeg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 h = img.shape[0]
 w = img.shape[1]
 
-
 print(h, w)
 
-plt.imshow(img)
+region_of_interest_vertex = np.array([[
+    (0, h),
+    (w/2, h/2 + 30),
+    (w, h)
+]], np.int32)
+
+mask = np.zeros_like(img)
+channel_count = img.shape[2]
+match_mask = (255,) * channel_count
+cv.fillPoly(mask, region_of_interest_vertex, match_mask)
+masked_image = cv.bitwise_and(img, mask)
+
+plt.imshow(masked_image)
 plt.show()
